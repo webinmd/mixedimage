a77851f9dec49b8793238eeca119d116564e5cb0
Webinmd <89poilo@gmail.com>
2017-01-04 13:27:51 +0200

    Добавлены новые события

--- core/components/mixedimage/elements/plugins/plugin.mixedImage.php
+++ core/components/mixedimage/elements/plugins/plugin.mixedImage.php
@@ -1,18 +1,51 @@
 <?php
 $corePath = $modx->getOption('core_path',null,MODX_CORE_PATH).'components/mixedimage/';
+$assetsUrl = $modx->getOption('assets_url',null,MODX_ASSETS_URL).'components/mixedimage/';
+
+$modx->lexicon->load('mixedimage:default');
+
 switch ($modx->event->name) {
     case 'OnTVInputRenderList':
-        $modx->event->output($corePath.'tv/input/');
+        $modx->event->output($corePath.'elements/tv/input/');
         break;
     case 'OnTVOutputRenderList':
-        $modx->event->output($corePath.'tv/output/');
+        $modx->event->output($corePath.'elements/tv/output/');
         break;
     case 'OnTVInputPropertiesList':
-        $modx->event->output($corePath.'tv/inputoptions/');
+        $modx->event->output($corePath.'elements/tv/input/options/');
         break;
     case 'OnTVOutputRenderPropertiesList':
-        $modx->event->output($corePath.'tv/properties/');
+        $modx->event->output($corePath.'elements/tv/properties/');
         break;
     case 'OnManagerPageBeforeRender':
+        //$modx->controller->addLexiconTopic('mixedimage:default');
+        break;
+    case 'OnFileManagerUpload':
+        if ((bool)$modx->getOption('mixedimage.translit', null, false))
+        {
+            $fat = $modx->getOption('friendly_alias_translit');
+            $friendly_alias_translit = (empty($fat) || $fat == 'none') ? false : true;
+
+            foreach($files as $file)
+            {
+                if($file['error'] == 0)
+                {
+                    $pathInfo = pathinfo($file['name']);
+                    $oldPath = $directory.$file['name'];
+
+                    $filename = modResource::filterPathSegment($modx, $pathInfo['filename']); // cleanAlias (translate)
+                    if ($friendly_alias_translit)
+                    {
+                        $filename = preg_replace('/[^A-Za-z0-9_-]/', '', $filename); // restrict segment to alphanumeric characters only
+                    }
+                    $filename = preg_replace('/-{2,}/','-',$filename); // remove double symbol "-"
+                    $filename = trim($filename, '-'); // remove first symbol "-"
+
+                    $newPath = $filename . '.' . strtolower($pathInfo['extension']);
+
+                    $source->renameObject($oldPath, $newPath);
+                }
+            }
+        }
         break;
 }
